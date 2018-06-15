fe2f8e9c86f15b8165c4d33062d24021e424c090
David Athobe <dathobek@gmail.com>
2018-06-15 11:13:09 +0100

    Service-added

---
+++ src/app/Shared/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import {AngularFireAuth} from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/switchMap';
+
+@Injectable()
+export class AuthService {
+
+  constructor() { }
+
+}
