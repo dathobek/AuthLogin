b986aa1ca5b2fa6fb6f00d5768dc3a1e19050bab
David Athobe <dathobek@gmail.com>
2018-06-15 11:01:58 +0100

    Firebase-Integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import {FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.FirebaseConfig),
+    FormsModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  FirebaseConfig : {
+    apiKey: "AIzaSyBdTLzBcnUHTEKKImD8AovcCylUJkN0gDE",
+    authDomain: "angular-authentication-dc429.firebaseapp.com",
+    databaseURL: "https://angular-authentication-dc429.firebaseio.com",
+    projectId: "angular-authentication-dc429",
+    storageBucket: "angular-authentication-dc429.appspot.com",
+    messagingSenderId: "82788064118"
+  }
 };
